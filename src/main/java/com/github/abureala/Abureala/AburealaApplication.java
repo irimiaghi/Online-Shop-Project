d2fa42294dc1e6fe6d88f3ca18a5a176b05e570b
Percy <irimiaghi@gmail.com>
2019-03-02 10:12:45 +0200

    Initial commit

---
+++ src/main/java/com/github/abureala/Abureala/AburealaApplication.java
@@ -0,0 +1,13 @@
+package com.github.abureala.Abureala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AburealaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AburealaApplication.class, args);
+	}
+
+}
