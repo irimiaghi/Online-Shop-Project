ad450ca5d6e6997c6bd103a8598929f46c8acddc
Percy <irimiaghi@gmail.com>
2019-03-02 10:24:43 +0200

    Pom commit

--- src/main/java/com/github/abureala/Abureala/AburealaApplication.java
+++ src/main/java/com/github/abureala/Abureala/AburealaApplication.java
@@ -3,11 +3,13 @@ package com.github.abureala.Abureala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 @SpringBootApplication
 public class AburealaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AburealaApplication.class, args);
+		System.out.println("Hello World!");
 	}
-
 }
