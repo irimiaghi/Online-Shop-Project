38b52e3701b2ca43a81b2d3f4a2a061626f1cad5
Stefan Dulgheru <stefan.dulgheru@gmail.com>
2019-03-03 15:12:05 +0200

    SpringData pricing commit

--- src/main/java/com/github/abureala/Abureala/repositories/ProductPricingRepository.java
+++ src/main/java/com/github/abureala/Abureala/repositories/ProductPricingRepository.java
@@ -1,8 +1,11 @@
 package com.github.abureala.Abureala.repositories;
 
+import com.github.abureala.Abureala.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 @Repository
-public interface ProductPricingRepository {
-
+public interface ProductPricingRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findById(Long id);
 }
